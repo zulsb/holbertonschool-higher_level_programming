a06eb09da26ba56153a1cea40d702b2ec019cdda
Luz <zulsb2093@gmail.com>
2019-12-11 00:24:55 +0000

    tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+alpah = 1
 for alpah in range(122, 96, -1):
-    if alpah % 2 == 1:
+    if alpah % 2 == 0:
         alpah = alpah - 32
     print("{:c}".format(alpah), end="")
+    alpah = alpah + 1
