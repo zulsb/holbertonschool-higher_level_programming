256e66aae70ccd958e4b648def1496678f8f781c
Luz <zulsb2093@gmail.com>
2019-12-12 23:36:22 +0000

    Arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 1:
+        print("0 arguments")
+        exit()
+    arg_num = len(argv) - 1
+    if len(argv) == 2:
+        print("{} argument:".format(arg_num))
+    else:
+        print("{} argument:".format(arg_num))
+
+for num in range(1, len(argv)):
+    print("{}: {}".format(num, argv[num]))
