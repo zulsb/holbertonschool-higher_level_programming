364625f28f08b334a9745dcfc42ca9e704f72bae
Luz <zulsb2093@gmail.com>
2019-12-18 00:43:43 +0000

    Only by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     n_lis = my_list.copy()
-    for m in n_lis:
+    for m in range(len(n_lis)):
         if (my_list[m] % 2) == 0:
             n_lis[m] = True
         else:
