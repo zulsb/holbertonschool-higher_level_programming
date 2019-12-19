65a2c6fcf4e6393964fda5b79c153d7358dc2f17
Luz <zulsb2093@gmail.com>
2019-12-19 19:19:57 +0000

    Matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    n = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    n = [copy[:] for copy in matrix]
     for t in range(len(matrix)):
         for x in range(len(matrix[0])):
             n[t][x] = pow(matrix[t][x], 2)
