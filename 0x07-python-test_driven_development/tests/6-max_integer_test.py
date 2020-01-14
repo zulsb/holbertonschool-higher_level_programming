d4731046bc927c7e1509ee6feb4d00966cb37775
Luz <zulsb2093@gmail.com>
2020-01-14 02:00:07 +0000

    Max integer test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,6 +10,35 @@ class TestMaxInteger(unittest.TestCase):
         l = []
         self.assertEqual(max_integer(l), None)
 
+    def test_docstr(self):
+        self.assertIsNotNone(max_integer.__doc__)
+
+    def test_Normal(self):
+        l = [1, 2, 3, 4, 5]
+        self.assertEqual(max_integer(l), 5)
+
+    def test_elem(self):
+        l = [5]
+        self.assertEqual(max_integer(l), 5)
+
+    def test_str_int(self):
+        self.assertRaises(TypeError, max_integer, ["a", "b", 1])
+
+    def test_neg(self):
+        self.assertEqual(max_integer([-2, -1]), -1)
+
+    def test_basic_negative_zero_zero(self):
+        l = [0, -1]
+        self.assertEqual(max_integer(l), 0)
+
+    def test_wds(self):
+        l = ["hello", "hi", "bye"]
+        self.assertEqual(max_integer(l), "hi")
+
+    def test_dictor(self):
+        l = {0: "a", 1: "b", 2: "c"}
+        self.assertEqual(max_integer(l), "c")
+
 
 if __name__ == '__main__':
     unittest.main()
