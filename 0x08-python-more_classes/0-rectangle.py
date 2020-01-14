c2ef8c4ca32e2fe9c7afe1e320c6eef435a44f5e
Luz <zulsb2093@gmail.com>
2020-01-14 18:57:19 +0000

    Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+   Modulo contains an empty class
+   Rectangle that defines a rectangle.
+"""
+
+
+class Rectangle():
+    """ This is an empty class
+        that defines a rectangle.
+    """
+    pass
