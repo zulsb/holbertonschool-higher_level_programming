1d54f0861460637cc6a418f3a3a04a13bdcdb427
Luz <zulsb2093@gmail.com>
2020-01-20 23:34:22 +0000

    My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+    Module contains the MyList class.
+"""
+
+
 class MyList(list):
+    """ Subclass inherits from list. """
     def print_sorted(self):
+        """ Function to print list in sorted order. """
         print(sorted(self))
