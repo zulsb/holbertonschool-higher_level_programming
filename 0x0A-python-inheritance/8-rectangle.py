62d2e39ddd12e54775d41233c18ddc5306febbf8
Luz <zulsb2093@gmail.com>
2020-01-21 06:01:51 +0000

    Rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -18,9 +18,9 @@ class BaseGeometry():
             value: Value.
         """
         if not type(value) is int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
