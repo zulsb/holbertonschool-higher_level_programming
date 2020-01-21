bb93a6391913433296d293099a8648ccb83ea96e
Luz <zulsb2093@gmail.com>
2020-01-21 19:07:09 +0000

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Module contains the function read_file.
+"""
+
+
+def read_file(filename=""):
+    """ Reads a text file (UTF8) and prints it to stdout.
+        Args:
+            filename: File name to read.
+    """
+    with open(filename, mode="r", encoding="utf-8") as fl:
+        print(fl.read())
