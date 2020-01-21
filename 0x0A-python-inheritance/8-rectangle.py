540d81177b224a6dc6fe23f6fe3760aa9bd0e23f
Luz <zulsb2093@gmail.com>
2020-01-21 00:08:29 +0000

    Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+    Module contain the class BaseGeometry.
+"""
+
+
+class BaseGeometry():
+    """ Geometry class. """
+
+    def area(self):
+        """ Function area exception. """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Function integer validation.
+            Args:
+            name: Name value.
+            value: Value.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle class. """
+    def __init__(self, width, height):
+        """ Module inicializer.
+            Args:
+            width: Rectangle width.
+            height: Rectangle height.
+        """
+        if super().integer_validator("width", width):
+            self.__width = width
+        if super().integer_validator("height", height):
+            self.__height = height
