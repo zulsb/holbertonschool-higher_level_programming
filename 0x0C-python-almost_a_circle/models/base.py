cdb071ae343a92341bdde8f14339d4deee0782c6
Luz <zulsb2093@gmail.com>
2020-01-27 19:15:15 +0000

    Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,6 +26,7 @@ class Base():
             Args:
                 list_dictionaries: Is a list of dictionaries.
         """
-        if is not list_dictionaries or len(list_dictionaries) is 0:
+        if not list_dictionaries or list_dictionaries is None:
             return "[]"
-        return json.dumps(list_dictionaries)
+        else:
+            return json.dumps(list_dictionaries)
