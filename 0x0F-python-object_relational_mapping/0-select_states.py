69cfea9ce5072c19b2875665bd3a3a4769ca7979
Luz <zulsb2093@gmail.com>
2020-03-28 04:00:14 +0000

    Add script that lists all states from the database hbtn_0e_0_usa.

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    Script that lists all states from the database hbtn_0e_0_usa.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    """Connect to database."""
+    conec_db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
+
+    """Create cursor to exec queries using SQL."""
+    cursor = conec_db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    conec_db.close()
