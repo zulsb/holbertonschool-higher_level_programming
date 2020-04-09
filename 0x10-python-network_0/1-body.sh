8bb34edefaefab120f4e497a52d1993f289cb828
Luz <zulsb2093@gmail.com>
2020-04-09 22:45:44 +0000

    Comment correction.

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# cript that takes in a URL, sends a GET request to the URL
-# and displays the body of the response.
+# cript that takes in a URL, sends a GET request to the URL and displays the body of the response.
 curl -sL "$1"
