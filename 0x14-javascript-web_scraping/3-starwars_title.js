65159ffee8169a5ce75b3511f84ebb50ae3d6bf8
Luz <zulsb2093@gmail.com>
2020-05-13 01:09:28 +0000

    Add a script that prints the title of a Star Wars movie.

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-request('http://swapi.co/api/films/' + process.argv[2], function (err, response, body) {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
-    console.log(JSON.parse(body).title);
+    const tiJson = JSON.parse(body);
+    console.log(tiJson.title);
   }
 });
